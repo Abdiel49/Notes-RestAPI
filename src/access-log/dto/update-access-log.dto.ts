2733de3cb67a5845d5bee04012f5f55719820f1b
abdiel49 <abdielorellana3@gmail.com>
2024-04-12 00:30:50 -0400

    feature(create AccessLog Entity and update DTO's)

--- src/access-log/dto/update-access-log.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAccessLogDto } from './create-access-log.dto';
-
-export class UpdateAccessLogDto extends PartialType(CreateAccessLogDto) {}
--- src/access-log/entities/access-log.entity.ts
+++ src/access-log/entities/access-log.entity.ts
@@ -1,1 +1,41 @@
-export class AccessLog {}
+import { User } from 'src/user/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class AccessLog {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => User, (user) => user.accessLog)
+  user: string;
+
+  @Column('varchar')
+  lat: string;
+
+  @Column('varchar')
+  long: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  public deletedAt: Date;
+}
